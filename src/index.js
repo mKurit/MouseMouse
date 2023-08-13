e11513f6f8f68df558544253dbd47cd7bf6c47ed
K93MSY <masaya93@ogaki-tv.ne.jp>
2023-08-13 12:13:51 +0900

    fixed window setting

--- src/index.js
+++ src/index.js
@@ -17,11 +17,9 @@ const createWindow = () => {
     },
   });
 
+  mainWindow.setMenuBarVisibility(false);
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
